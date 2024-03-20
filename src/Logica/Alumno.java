041d2602632a174586f23d4c266cfceb14853924
Milton Figueredo Miles <gratno@gmail.com>
2024-03-20 13:53:59 -0500

    add setters and getters - of print window data alumno - user-

--- src/Logica/Alumno.java
+++ src/Logica/Alumno.java
@@ -15,6 +15,30 @@ public class Alumno {
         this.lastName = lastName;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     public void mostrarNombre() {
 
         System.out.println("Soy un Alumno de LATAM");
--- src/Logica/POO.java
+++ src/Logica/POO.java
@@ -2,14 +2,29 @@ package Logica;
 
 public class POO {
 
-
     public static void main(String[] args) {
 
         Alumno alumno1 = new Alumno();
         Alumno alumno2 = new Alumno(5, "Luisa", "Marcus");
-       // System.out.println(alumno2);
+        System.out.println("La ID del alumno 2 es: " + alumno2.getId());
+        System.out.println("El nombre es: " + alumno2.getName());
+        System.out.println("El apellido es: " + alumno2.getLastName());
+
+        alumno1.setId(11);
+        alumno1.setName("Lorena");
+        alumno1.setLastName("Hernandez");
+
+        System.out.println("----------------");
+        System.out.println("La ID del alumno 1 es: " + alumno1.getId());
+        System.out.println("El nombre es: " + alumno1.getName());
+        System.out.println("El apellido es: " + alumno1.getLastName());
+
+        System.out.println("----------------");
+        alumno2.setId(64);
+        System.out.println("La ID del alumno 2 es: " + alumno2.getId());
+        System.out.println("El nombre es: " + alumno2.getName());
+        System.out.println("El apellido es: " + alumno2.getLastName());
+
 
     }
-
-
 }
