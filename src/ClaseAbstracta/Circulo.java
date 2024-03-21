8457efa43226c9db46e732dc05ab4e5a4486dd5a
Milton Figueredo Miles <gratno@gmail.com>
2024-03-20 20:41:54 -0500

    Methods abstrac

--- src/ClaseAbstracta/Circulo.java
+++ src/ClaseAbstracta/Circulo.java
@@ -1,4 +1,24 @@
 package ClaseAbstracta;
 
 public class Circulo extends Figura {
+
+
+    private double radio;
+
+    public Circulo() {
+    }
+
+    public Circulo(double x, double y, double radio) {
+        super(x, y);
+        this.radio = radio;
+    }
+
+    @Override
+    public double calcularArea() {
+
+        double pi = 3.4;
+        double resultado = pi * radio * radio;
+
+        return resultado;
+    }
 }
