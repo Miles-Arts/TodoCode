f0c947d512649df7c142e3e9d43831b4edd58474
Milton Figueredo Miles <gratno@gmail.com>
2024-03-22 15:13:43 -0500

    Add person of the ArrayList, example

---
+++ src/ArrayList/ArrayLists.java
@@ -0,0 +1,21 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ArrayLists {
+
+    public static void main(String[] args) {
+
+
+        List<Persona> lista = new ArrayList<Persona>();
+
+        lista.add(new Persona(1, "Carol Ruiz", 24 ));
+        lista.add(new Persona(2, "Margarita Molina", 22));
+        lista.add(new Persona(3, "Sonia Ruiz", 26));
+        lista.add(new Persona(4, "Lina Molina", 23));
+
+
+    }
+}
