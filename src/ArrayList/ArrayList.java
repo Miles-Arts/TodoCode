b7c87ff5d1118a2aeeae5772d78dd08f6838189d
Milton Figueredo Miles <gratno@gmail.com>
2024-03-22 14:58:37 -0500

    example ArrayList and Person

--- src/ArrayList/ArrayList.java
+++ src/ArrayList/ArrayList.java
@@ -1,11 +1,15 @@
 package ArrayList;
 
-public class ArrayList {
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ArrayLists {
 
     public static void main(String[] args) {
 
 
-
+        List<Persona> lista = new ArrayList<Persona>();
 
     }
 }
