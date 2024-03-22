d57fdb613349e40700b8002c6c09b09764da6fb8
Milton Figueredo Miles <gratno@gmail.com>
2024-03-21 22:04:52 -0500

    create object in MAIN

--- src/Pokemon/EjercicioIntegrador.java
+++ src/Pokemon/EjercicioIntegrador.java
@@ -4,7 +4,22 @@ public class EjercicioIntegrador {
 
     public static void main(String[] args) {
 
+        Squirtle squirtle = new Squirtle();
+        Charmander charmander = new Charmander();
+        Bulbasaur bulbasaur = new Bulbasaur();
+        Pikachu pikachu = new Pikachu();
 
+        squirtle.atacarAraniazo();
+        squirtle.atacarHidroBomba();
+
+        charmander.atacarAraniazo();
+        charmander.atacarLanzaLlamas();
+
+        bulbasaur.atacarAraniazo();
+        bulbasaur.atacarDrenaje();
+
+        pikachu.atacarAraniazo();
+        pikachu.atacarImpactrueno();
 
     }
 }
