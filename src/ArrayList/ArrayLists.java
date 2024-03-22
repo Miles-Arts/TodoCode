9d74feb0092a4ca750488094f63411ee1fecf5db
Milton Figueredo Miles <gratno@gmail.com>
2024-03-22 15:34:59 -0500

    Add person of the ArrayList, example print of the window

--- src/ArrayList/ArrayLists.java
+++ src/ArrayList/ArrayLists.java
@@ -16,6 +16,19 @@ public class ArrayLists {
         lista.add(new Persona(3, "Sonia Ruiz", 26));
         lista.add(new Persona(4, "Lina Molina", 23));
 
+        //RECORRER POR ÍNDICE
 
+        System.out.println("---------FOR----------");
+        for(int i = 0; i < lista.size(); i++  ) {
+            System.out.println("Prueba " + lista.get(i).getNombre());
+        }
+
+        System.out.println();
+        System.out.println("---------FOREACH----------");
+        //RECORRIDO FOREACH
+
+        for (Persona persona : lista) {
+            System.out.println("Revisión: " + persona.getNombre());
+        }
     }
 }
