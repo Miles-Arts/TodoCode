2c76a72f1a2f23e31396bc78893e3c65f1fced48
Milton Figueredo Miles <gratno@gmail.com>
2024-03-28 13:41:39 -0500

    Add list an user

--- src/LinkedLists/LinkedLists.java
+++ src/LinkedLists/LinkedLists.java
@@ -9,11 +9,17 @@ public class LinkedLists {
 
         List<Persona> lista = new ArrayList<Persona>();
 
+        //Agregar persona al final de la lista
+        lista.add(new Persona(1, "Luisa Andrea", 24 ));
+        lista.add(new Persona(2, "Sol Camila", 29));
+        lista.add(new Persona(3, "Paula Joana", 22));
+        lista.add(new Persona(3, "Nataly Sofia", 27));
 
-        lista.add(new Persona(1, "Luisa", 24 ));
-        lista.add(new Persona(2, "Camila", 29));
-        lista.
-
+        System.out.println("------FOREACH------");
+        //Recorrido foreach
+            for(Persona persona:lista){
+                System.out.println("Prueba " + persona.getNombre());
+            }
 
 
     }
