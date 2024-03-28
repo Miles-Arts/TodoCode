6cee1af806be0ce38ba65afc8716ad9f69c3354b
Milton Figueredo Miles <gratno@gmail.com>
2024-03-28 17:48:49 -0500

    REcorreidno Pila STACK push, peek, isEmpty, pop, search

--- src/Pilas/Pilas.java
+++ src/Pilas/Pilas.java
@@ -8,7 +8,7 @@ public class Pilas {
 
         Stack<Integer> pila = new Stack<Integer>();
 
-        System.out.println("Lista vacía: " + pila);
+        System.out.println("Pila vacía: " + pila);
         System.out.println("Está vacía: " + pila.isEmpty());
 
         //Agregar
@@ -17,6 +17,7 @@ public class Pilas {
         pila.push(3);
         pila.push(4);
         pila.push(5);
+        //pila.push(5);
 
         //ForEach REcorrido
         for(Integer pilita : pila) {
@@ -24,10 +25,11 @@ public class Pilas {
         }
 
         //Mostrar
-
         System.out.println("Pila: " + pila);
         System.out.println("¿Pila vacía? " + pila.isEmpty());
 
-
+        pila.pop(); //Eliminar el último elemento de la Pila STACK
+        System.out.println("¿Esta el 3? " + pila.search(3));
+        System.out.println("Último agregado: " + pila.peek());
     }
 }
