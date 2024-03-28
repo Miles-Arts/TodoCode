1cd62ae390b9fced1e0a84d7f75f775b380c0698
Milton Figueredo Miles <gratno@gmail.com>
2024-03-28 13:37:04 -0500

    new week project

---
+++ src/LinkedLists/LinkedLists.java
@@ -0,0 +1,21 @@
+package LinkedLists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinkedLists {
+
+    public static void main(String[] args) {
+
+        List<Persona> lista = new ArrayList<Persona>();
+
+
+        lista.add(new Persona(1, "Luisa", 24 ));
+        lista.add(new Persona(2, "Camila", 29));
+        lista.
+
+
+
+    }
+
+}
