0f145058dc61f63f4188afb21a06363004267f61
Milton Figueredo Miles <gratno@gmail.com>
2024-03-28 14:51:58 -0500

    Example delete ArrayList and LinkeList

--- src/ExampleArrayAndLinkedList/LinkedListz.java
+++ src/ExampleArrayAndLinkedList/LinkedListz.java
@@ -10,10 +10,14 @@ public class LinkedListz {
 
         List<Personas> listaArray = new ArrayList<Personas>();
 
+        //System.out.println("----ArrayList----");
         listaArray.add(new Personas(1, "Ana", 20));
         listaArray.add(new Personas(2, "Juanita", 22));
         listaArray.add(new Personas(3, "Luciana", 30));
         listaArray.add(new Personas(4, "Sol", 25));
+
+        //System.out.println(listaArray.getNombre());
+        //System.out.println("----LinkedList----");
 
 
         List<Personas> listaLinked = new LinkedList<Personas>();
@@ -21,6 +25,7 @@ public class LinkedListz {
         listaLinked.add(new Personas(2, "Juanita", 22));
         listaLinked.add(new Personas(3, "Luciana", 30));
         listaLinked.add(new Personas(4, "Sol", 25));
+        System.out.println(listaLinked);
 
         //Remove ArrayList number 1
         listaArray.remove(1);
@@ -29,9 +34,20 @@ public class LinkedListz {
         for(Personas persona1: listaLinked) {
             if(persona1.getNombre().equals(nombreBorrar)){
                 listaLinked.remove(persona1);
-                break;
+                break; //BREAK - Romper para que Java reordene los punteros y volver a recorrer el LinkedList si se necesita
             }
         }
-        System.out.println("----To END----");
+        System.out.println("----After To Removed----");
+        //Recorrido FOREACH
+        System.out.println("------ArrayList------");
+            for(Personas persona : listaArray) {
+                System.out.println("Prueba " + persona.getNombre());
+            }
+
+        System.out.println();
+        System.out.println("------LinkedList------");
+            for(Personas persona : listaLinked) {
+                System.out.println("Prueba " + persona.getNombre());
+            }
     }
 }
