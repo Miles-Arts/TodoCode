42a200586f2f466679d45f234435bb58c771f809
Milton Figueredo Miles <gratno@gmail.com>
2024-03-30 13:43:09 -0500

    Example try and ctach

---
+++ src/Exception/Excepciones.java
@@ -0,0 +1,33 @@
+package Exception;
+
+public class Excepciones {
+
+    public static void main(String[] args) {
+
+       // int resultado = 3/0;
+
+        //Crear el try
+
+   /*     try {
+            int resultado = 3/0;
+        } catch (Exception e) {
+
+            System.out.println("----Exception:----");
+            System.out.println("No se puede dividir por CERO.");
+
+        } finally {
+            System.out.println("Salir");
+        }*/
+
+        //VECTOR
+        int edades [] = {25, 19, 29, 34};
+
+
+
+        try {
+            System.out.println("La edad de la posición 4 es: " + edades[4]);
+        } catch (Exception e) {
+            System.out.println("Este índice no existe: " + edades[4]);
+        }
+    }
+}
