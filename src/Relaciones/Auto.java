4f6f8c922fef29cc83eb32117779b0482961d7bd
Milton Figueredo Miles <gratno@gmail.com>
2024-03-30 18:49:19 -0500

    RELACIONES entre CLASES en JAVA POO 🖥️ | 1 a 1 y 1 a N

--- src/Relaciones/Auto.java
+++ src/Relaciones/Auto.java
@@ -1,10 +1,31 @@
 package Relaciones;
 
+import java.util.List;
+
 public class Auto {
 
     private Long id;
     private String marca;
     private String modelo;
+
+    private List<Propietario> listaPropietarios;
+
+    public List<Propietario> getListaPropietarios() {
+        return listaPropietarios;
+    }
+
+    public void setListaPropietarios(List<Propietario> listaPropietarios) {
+        this.listaPropietarios = listaPropietarios;
+    }
+
+    public Auto(Long id, String marca, String modelo, List<Propietario> listaPropietarios) {
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.listaPropietarios = listaPropietarios;
+    }
+//private Propietario prop;  //Declarar un OBJETO de otra class
+    //private Propietario propietario = new Propietario();
 
     public Auto() {
     }
@@ -14,6 +35,21 @@ public class Auto {
         this.marca = marca;
         this.modelo = modelo;
     }
+
+ /*   public Auto(Long id, String marca, String modelo, Propietario prop) {
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.prop = prop;
+    }
+
+    public Propietario getProp() {
+        return prop;
+    }
+
+    public void setProp(Propietario prop) {
+        this.prop = prop;
+    }*/
 
     public Long getId() {
         return id;
--- src/Relaciones/Relaciones.java
+++ src/Relaciones/Relaciones.java
@@ -1,4 +1,35 @@
 package Relaciones;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Relaciones {
+
+    public static void main(String[] args) {
+
+        Auto automovil = new Auto();
+
+        automovil.setId(1L); //L mayuscula por LONG
+        automovil.setMarca("Mercedes Benz");
+        automovil.setModelo("S600 - W140");
+
+        List<Propietario> listaPropietarios = new ArrayList<Propietario>();
+
+        Propietario propietario1 = new Propietario();
+        Propietario propietario2 = new Propietario();
+
+        propietario1.setId(20L);
+        propietario1.setName("Luciana Mirrol");
+        propietario1.getLastName("Mirrol");
+
+        propietario2.setId(21L);
+        propietario2.setName("Luca");
+        propietario2.getLastName("Rittovi");
+
+        listaPropietarios.add(propietario1);
+        listaPropietarios.add(propietario2);
+
+        automovil.setListaPropietarios(listaPropietarios);
+
+    }
 }
