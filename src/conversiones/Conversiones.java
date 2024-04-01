d3f09846912d2de54b9379a37d8d342fcfe11507
Milton Figueredo Miles <gratno@gmail.com>
2024-03-31 19:53:45 -0500

    ValueOf, ParseInt, pardeDouble and others

--- src/conversiones/Conversiones.java
+++ src/conversiones/Conversiones.java
@@ -24,7 +24,7 @@ public class Conversiones {
         System.out.println("Número Long: " + numeroLong);
         //System.out.println(numeroString);*/
 
-        String cantidad = "15";
+       /* String cantidad = "15";
         String precio = "150.27";
 
         //PARSEO convertir String a INT
@@ -47,7 +47,17 @@ public class Conversiones {
         System.out.println("Precio Double: " + precio);
 
         System.out.println("Valor de la compra: $" + totalCompra);
-        System.out.println("Total a pagar: $" + (precioDouble * cantidadEntero));
+        System.out.println("Total a pagar: $" + (precioDouble * cantidadEntero));*/
 
+
+        //NUMERICOS a STRING
+        int edad = 30;
+        double estatura = 1.67;
+
+        String edadString = String.valueOf(edad);
+        String estaturaString = String.valueOf(estatura);
+
+        System.out.println("Pasar de  INT a STRING: " + edadString);
+        System.out.println("Pasar de  DOUBLE a STRING: " + estaturaString);
     }
 }
