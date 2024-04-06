44add1ef3ea587acd7b32cff6b33642ae7d3462d
Milton Figueredo Miles <gratno@gmail.com>
2024-04-06 14:19:08 -0500

    Converiones, log, int, double,String

---
+++ src/conversioness/Conversiones.java
@@ -0,0 +1,22 @@
+package conversioness;
+
+public class Conversiones {
+
+    public static void main(String[] args) {
+
+
+        double num = 1.67;
+
+        int numInt = (int) num;
+
+        long numLog = (long) num;
+
+
+        System.out.println(num);
+        System.out.println(numInt);
+        System.out.println(numLog);
+
+
+
+    }
+}
